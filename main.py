37e9fe2eb3f0a1eb8df1377d88ab9f34bcdb340d
MRomano26 <michael.mar26@gmail.com>
2021-11-11 17:50:46 -0500

    Fixed requirements so voice related functions work

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
+import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="?")
 
-client.run("")
+client = commands.Bot(command_prefix="!")
+
+@client.command()
+async def play(ctx, url : str):
+    try:
+        voiceChannel = ctx.author.voice.channel
+        await voiceChannel.connect()
+    except AttributeError:
+        return await ctx.send("Maybe try being in a channel mate.")
+
+client.run("OTA4NDczMTg5NTE5MjIwNzM2.YY2Plg.Wm4IEYVayjCAoUVhjouKWd3Fy6U")

588c79915cbbc6d26e0b9602b0cae48d16b8dce1
MRomano26 <michael.mar26@gmail.com>
2021-11-11 19:15:07 -0500

    Made command !play play music from a youtube url after joining channel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
-from discord import voice_client
+from discord.errors import ClientException
+import youtube_dl
 from discord.ext import commands
 
 
@@ -8,14 +9,29 @@ client = commands.Bot(command_prefix="!")
 
 @client.command()
 async def play(ctx, url : str):
+
     try:
         voiceChannel = ctx.author.voice.channel
+        await voiceChannel.connect()
         voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-        if not voice.is_connected():
-            await voiceChannel.connect()
     except AttributeError:
         return await ctx.send("Maybe try being in a channel mate.")
-    # we'll add music playing functionality later
+    except ClientException:
+        return
+    
+    song_there = os.path.isfile("song.webm")
+    if song_there:
+        os.remove("song.webm")
+    
+    ydl_opts = {
+        'format' : '249/250/251'
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".webm"):
+            os.rename(file, "song.webm")
+    voice.play(discord.FFmpegOpusAudio("song.webm"))
 
 @client.command()
 async def leave(ctx):
